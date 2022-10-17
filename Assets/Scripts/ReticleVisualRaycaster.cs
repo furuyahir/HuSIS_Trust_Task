7fe0da45b314e6697af24e8e0a3e09a7fd02059b
Hiroshi Furuya <hfuruya@knights.ucf.edu>
2022-10-17 14:52:29 -0400

    Added reticle assets

---
+++ Assets/Scripts/ReticleVisualRaycaster.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleVisualRaycaster : MonoBehaviour
+{
+    public string RaycastLayerName = "HuSISWalls";
+    public GameObject Visual;
+    public Transform RaySourceTransform;
+    public Transform VisualLookAtTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (VisualLookAtTransform == null)
+        {
+            VisualLookAtTransform = RaySourceTransform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        int layer = 1 << LayerMask.NameToLayer(RaycastLayerName);
+        if (Physics.Raycast(RaySourceTransform.position, RaySourceTransform.forward, out hitInfo, Mathf.Infinity, layer))
+        {
+            Visual.transform.position = hitInfo.point;
+            Visual.transform.LookAt(VisualLookAtTransform);
+        }    
+    }
+}
